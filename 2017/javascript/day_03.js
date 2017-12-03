97f367578c32b41f2c880204e6c3f4012453514d
David Zukowski <zukoweb@gmail.com>
2017-12-03 13:33:34 -0600

    refactor(day_03): use generators for spirals

--- 2017/javascript/day_03.js
+++ 2017/javascript/day_03.js
@@ -1,39 +1,37 @@
+const assert = require('assert')
 const _ = require('redash')
 
 const INPUT = 277678
 
-const draw = (layers) => {
-  console.log(layers.map(l => JSON.stringify(l)))
-}
-
 // Star #1
 // -----------------------------------------------
-const exercise_01 = (target) => {
-  const layers = []
-  const addLayer = () => {
-    if (!layers.length) {
-      layers.push([1])
-      return
-    }
-
-    const sideLen = _.inc(2 * layers.length)
-    const start = _.last(_.last(layers))
-    layers.push(_.range(start + 1, start + ((sideLen * 4) - 4) + 1))
+function *genSpiralLayers () {
+  let layer, layerSize = layerStart = 1
+  while (true) {
+    yield layer = _.range(layerStart, layerStart + layerSize)
+    layerStart = _.last(layer)
+    layerSize += 8
   }
-
-  while (!_.contains(target, _.last(layers) || [])) addLayer()
-  const layer = _.last(layers)
-  layer.unshift(layer.pop())
-
-  const rotate = 2 * (layers.length - 1)
-  const centers = [rotate / 2]
-  centers.push(centers[0] + rotate)
-  centers.push(centers[1] + rotate)
-  centers.push(centers[2] + rotate)
-
-  const idx = layer.indexOf(target)
-  const offset = Math.min.apply(Math, centers.map(c => Math.abs(c - idx))) - 1
-  return offset + layers.length
 }
 
-console.log(exercise_01(INPUT))
+const exercise_01 = (target) => {
+  let depth = 0, layer
+  for (layer of genSpiralLayers()) {
+    if (_.last(layer) >= target) break
+    depth++
+  }
+
+  const pivot = Math.ceil((layer.length - 1) / 4)
+  const start = pivot / 2
+  const centers = [start].concat(_.times(n => start + (pivot * (n + 1)), 3))
+
+  const idx = layer.indexOf(target)
+  const lateralOffset = Math.min.apply(Math, centers.map(c => Math.abs(c - idx)))
+  return lateralOffset + depth
+}
+
+assert.equal(exercise_01(1), 0)
+assert.equal(exercise_01(12), 3)
+assert.equal(exercise_01(23), 2)
+assert.equal(exercise_01(1024), 31)
+assert.equal(exercise_01(277678), 475)

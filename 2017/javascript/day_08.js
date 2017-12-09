f9443dfc851e217b7ffd58c565b44ab78dd8ae34
David Zukowski <zukoweb@gmail.com>
2017-12-09 13:15:15 -0600

    refactor(day_08): code golf star #2

--- 2017/javascript/day_08.js
+++ 2017/javascript/day_08.js
@@ -2,6 +2,8 @@ const assert = require('assert')
 const _ = require('redash')
 const fs = require('fs')
 const INPUT = fs.readFileSync('../inputs/day_08.txt', 'utf8')
+  .split('\n')
+  .filter(s => s.trim())
 
 const parseInstruction = (str) => {
   const [target, op, opVal, __, ...cond] = str.split(' ')
@@ -31,37 +33,38 @@ const runInstruction = (registers, { target, op, opAmt, cond }) => {
 
 // Star #1
 // -----------------------------------------------
-const exercise_01 = (input) => {
-  const instructions = input.split('\n').filter(s => s.trim()).map(parseInstruction)
-  const registers = instructions.reduce(runInstruction, new Map())
-  return Math.max(...(registers.values()))
-}
+const exercise_01 = _.pipe([
+  _.map(parseInstruction),
+  instructions => _.reduce(runInstruction, new Map(), instructions),
+  registers => Math.max(...registers.values()),
+])
 
-assert.equal(exercise_01(`b inc 5 if a > 1
-a inc 1 if b < 5
-c dec -10 if a >= 1
-c inc -20 if c == 10`), 1)
+assert.equal(exercise_01([
+  'b inc 5 if a > 1',
+  'a inc 1 if b < 5',
+  'c dec -10 if a >= 1',
+  'c inc -20 if c == 10',
+]), 1)
 
 assert.equal(exercise_01(INPUT), 5215)
 
 // Star #2
 // -----------------------------------------------
-const exercise_02 = (input) => {
-  let highest = -Infinity
-  const instructions = input.split('\n').filter(s => s.trim()).map(parseInstruction)
+const exercise_02 = _.pipe([
+  _.map(parseInstruction),
+  instructions => {
+    let registers = new Map()
+    return Math.max(...instructions.map((ins) => {
+      return Math.max(...(registers = runInstruction(registers, ins)).values())
+    }, registers))
+  },
+])
 
-  instructions.reduce((acc, ins) => {
-    const registers = runInstruction(acc, ins)
-    const _highest = Math.max(...registers.values())
-    highest = _highest > highest ? _highest : highest
-    return registers
-  }, new Map())
-  return highest
-}
-
-assert.equal(exercise_02(`b inc 5 if a > 1
-a inc 1 if b < 5
-c dec -10 if a >= 1
-c inc -20 if c == 10`), 10)
+assert.equal(exercise_02([
+  'b inc 5 if a > 1',
+  'a inc 1 if b < 5',
+  'c dec -10 if a >= 1',
+  'c inc -20 if c == 10',
+]), 10)
 
 assert.equal(exercise_02(INPUT), 6419)

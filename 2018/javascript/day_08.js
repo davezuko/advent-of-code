b8d3cb89e03a71ba12e7d400ffb0c162ef77917e
David Zukowski <dazukows@microsoft.com>
2019-11-08 15:56:39 -0800

    2018: solve day 8, part 1 (javascript)

---
+++ 2018/javascript/day_08.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+const path = require("path")
+const assert = require("assert")
+
+function main() {
+  const input = fs.readFileSync(path.resolve(__dirname, "./../inputs/day_08.txt"), "utf8")
+  assert.equal(star_01(input), 36027)
+}
+
+function star_01(input) {
+  let chars = input.split(" ").map(num => Number.parseInt(num))
+  let metadata = 0
+  let i = 0
+  function walk_children() {
+    let num_children = chars[i++]
+    let num_metadata = chars[i++]
+    while (num_children > 0) {
+      walk_children()
+      num_children--
+    }
+    while (num_metadata > 0) {
+      metadata += chars[i++]
+      num_metadata--
+    }
+  }
+  walk_children()
+  return metadata
+}
+
+main()

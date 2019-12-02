8dd6a2e08e3fd40c4f54aad57872f1a65d27c58a
David Zukowski <david@zuko.me>
2019-12-02 10:55:00 -0800

    2019: day 2 [part 2] (go)

--- 2019/go/day_02.go
+++ 2019/go/day_02.go
@@ -10,15 +10,38 @@ import (
 func main() {
 	dat, _ := ioutil.ReadFile("2019/inputs/day_02.txt")
 	ops := parseOpcodes(dat)
-
-	// Reset the program to the "1202 program alarm" state
-	ops[1] = 12
-	ops[2] = 2
-
-	star1(ops)
+	fmt.Printf("Star 1: %v\n", Star1(clone(ops)))
+	fmt.Printf("Star 2: %v\n", Star2(clone(ops)))
 }
 
-func star1(ops []int) {
+func Star1(ops []int) int {
+	ops[1] = 12
+	ops[2] = 2
+	return runProgram(ops)
+}
+
+func Star2(ops []int) int {
+	res := make(chan int)
+	for i := 0; i <= 99; i++ {
+		for j := 0; j <= 99; j++ {
+			go func(ops []int, i int, j int) {
+				defer func() {
+					if err := recover(); err != nil {
+						return
+					}
+				}()
+				ops[1] = i
+				ops[2] = j
+				if runProgram(ops) == 19690720 {
+					res <- 100*i + j
+				}
+			}(clone(ops), i, j)
+		}
+	}
+	return <-res
+}
+
+func runProgram(ops []int) int {
 	i := 0
 	exited := false
 	for exited != true {
@@ -38,7 +61,7 @@ func star1(ops []int) {
 		}
 		i += 4
 	}
-	fmt.Printf("%v\n", ops)
+	return ops[0]
 }
 
 func parseOpcodes(input []byte) []int {
@@ -48,4 +71,10 @@ func parseOpcodes(input []byte) []int {
 		ops = append(ops, num)
 	}
 	return ops
+}
+
+func clone(ops []int) []int {
+	clone := make([]int, len(ops))
+	copy(clone, ops)
+	return clone
 }

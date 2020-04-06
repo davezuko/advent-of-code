65784738299ab8117c0b1d9ef5926b92d61d4481
David Zukowski <david@zuko.me>
2020-04-06 15:11:30 -0500

    2019: reimplement day 1 with iterators (rust)

--- 2019/rust/src/day_01.rs
+++ 2019/rust/src/day_01.rs
@@ -1,31 +1,41 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
-pub fn run() {
-    let file = File::open("../inputs/day_01.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let mut total_fuel = 0;
-    let mut total_fuel_recursive = 0;
-
-    for line in reader.lines() {
-        let mass: i32 = line.unwrap().parse().unwrap();
-        total_fuel += calculate_fuel(mass);
-        total_fuel_recursive += calculate_fuel_recursive(mass);
-    }
-    println!("star 1: {}\nstar 2: {}", total_fuel, total_fuel_recursive);
+/// Returns the amount of fuel needed to support `mass`.
+pub fn calculate_fuel(mass: i32) -> i32 {
+    ((mass as f64 / 3.0).trunc() - 2.0) as i32
 }
 
-fn calculate_fuel(mass: i32) -> i32 {
-    return ((mass as f64 / 3.0).trunc() - 2.0) as i32;
+/// Returns the amount of fuel needed to support `mass` and the fuel itself.
+pub fn calculate_fuel_rec(mass: i32) -> i32 {
+    let fuel = calculate_fuel(mass);
+    if fuel > 0 {
+        fuel + calculate_fuel_rec(fuel)
+    } else {
+        0
+    }
 }
 
-fn calculate_fuel_recursive(mass: i32) -> i32 {
-    let mut fuel = 0;
-    let mut mass = calculate_fuel(mass);
-    while mass > 0 {
-        fuel += mass;
-        mass = calculate_fuel(mass);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::io::{BufRead, BufReader};
+
+    fn get_input_masses() -> impl Iterator<Item = i32> {
+        let file = fs::File::open("../inputs/day_01.txt").unwrap();
+        let reader = BufReader::new(file);
+        reader
+            .lines()
+            .map(|line| line.unwrap().parse::<i32>().unwrap())
     }
-    return fuel;
+
+    #[test]
+    fn test_star_1() {
+        let fuel: i32 = get_input_masses().map(calculate_fuel).sum();
+        assert_eq!(fuel, 3514064);
+    }
+
+    #[test]
+    fn test_star_2() {
+        let fuel: i32 = get_input_masses().map(calculate_fuel_rec).sum();
+        assert_eq!(fuel, 5268207);
+    }
 }
